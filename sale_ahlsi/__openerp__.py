2017db8a98a4c0c5d47cf1d3e14a88c94d52b690
Jason Concepcion <jsncon01@gmail.com>
2018-02-07 12:08:55 +0800

    first template commit

---
+++ sale_ahlsi/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "AHLSI Sale",
+    'version': '8.0.0.0.1',
+    'author': "Deutsche Motorgerate Inc.",
+    'website': "http://www.dmi.com.ph",
+    'category': 'stock',
+    'depends': [
+		'sale'
+    ],
+    'data': [
+    ],
+    
+    'installable': True,
+    'auto_install': False,
+}
